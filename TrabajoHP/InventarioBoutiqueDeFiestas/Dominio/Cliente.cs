0340b49642f09363023255295ca1e49ceaa3e73e
ezequiel klug <ezeklug@gmail.com>
2020-08-25 14:49:48 -0300

    Clase Cliente

---
+++ TrabajoHP/InventarioBoutiqueDeFiestas/Dominio/Cliente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventarioBoutiqueDeFiestas.Dominio
+{
+
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Direccion { get; set; }
+        public int Telefono { get; set; }
+        public string Email { get; set; }
+
+        public Cliente(int pId, string pNombre, string pApellido, string pDireccion, int pTelefono, string pEmail)
+        {
+            Id = pId;
+            Nombre = pNombre;
+            Apellido = pApellido;
+            Direccion = pDireccion;
+            Telefono = pTelefono;
+            Email = pEmail;
+        }
+    }
+}
