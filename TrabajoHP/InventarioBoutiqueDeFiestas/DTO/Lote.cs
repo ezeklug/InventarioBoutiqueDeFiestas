efc8e48af7f617d5c27016d92a26d332a811e1ac
matias quaroni <matu_quaroni@hotmail.com>
2020-08-28 16:24:23 -0300

    DTO Cliente y Lote

---
+++ TrabajoHP/InventarioBoutiqueDeFiestas/DTO/Lote.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventarioBoutiqueDeFiestas.DTO
+{
+    class Lote
+    {
+        public int Id { get; set; }
+        public int CantidadProductos { get; set; }
+        public DateTime FechaCompra { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+        // Hay que chequear si estado es un boolean o una enum
+        public Boolean Vencido { get; set; }
+        public int IdProducto { get; set; }
+    }
+}
