ebdea69972603cd9c8bf72009493242e022b1e5a
victoriaponsiano <56657579+victoriaponsiano@users.noreply.github.com>
2020-08-28 15:41:51 -0300

    .

--- TrabajoHP/InventarioBoutiqueDeFiestas/Controladores/ControladorCliente.cs
+++ TrabajoHP/InventarioBoutiqueDeFiestas/Controladores/ControladorCliente.cs
@@ -5,8 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace InventarioBoutiqueDeFiestas.Controladores
-{
+{ 
     class ControladorCliente
     {
+    
     }
 }
--- TrabajoHP/InventarioBoutiqueDeFiestas/Controladores/ControladorFachada.cs
+++ TrabajoHP/InventarioBoutiqueDeFiestas/Controladores/ControladorFachada.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using InventarioBoutiqueDeFiestas.Controladores;
 
 namespace InventarioBoutiqueDeFiestas.Controladores
 {
     class ControladorFachada
     {
-
+        ControladorPresupuesto controladorPresupuesto;
+        ControladorCliente controladorCliente;
+        ControladorProducto controladorProducto;
+        
+            
 
     }
+}
