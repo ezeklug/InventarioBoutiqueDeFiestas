669c6653c600af2915720825b0a7e9375ad0e08a
victoriaponsiano <56657579+victoriaponsiano@users.noreply.github.com>
2020-08-28 16:17:04 -0300

    ProductoDTO

--- TrabajoHP/InventarioBoutiqueDeFiestas/DTO/ProductoDTO.cs
+++ TrabajoHP/InventarioBoutiqueDeFiestas/DTO/ProductoDTO.cs
@@ -8,5 +8,14 @@ namespace InventarioBoutiqueDeFiestas.DTO
 {
     class ProductoDTO
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int StockMinimo { get; set; }
+        public int CantidadEnStock { get; set; }
+        public double PorcentajeDeGanancia { get; set; }
+        public double PrecioDeCompra { get; set; }
+
+        public int IdCategoria { get; set; }
     }
 }
