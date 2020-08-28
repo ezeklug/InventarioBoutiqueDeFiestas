5106b236ae317225cc2ff5aab3d8f907f15bff2e
victoriaponsiano <56657579+victoriaponsiano@users.noreply.github.com>
2020-08-28 16:27:13 -0300

    Agregué DTO

---
+++ TrabajoHP/InventarioBoutiqueDeFiestas/DTO/SeniaDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventarioBoutiqueDeFiestas.DTO
+{
+    class SeniaDTO
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public double Monto { get; set; }
+        public int IdPresupuesto { get; set; }
+    }
+}
---
+++ TrabajoHP/InventarioBoutiqueDeFiestas/DTO/VentaDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventarioBoutiqueDeFiestas.DTO
+{
+    class VentaDTO
+    {
+        public int Id { get; set; }
+        public DateTime FechaDeVenta { get; set; }
+
+        public int IdPresupuesto { get; set; }
+    }
+}
