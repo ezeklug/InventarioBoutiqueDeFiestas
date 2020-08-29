25773c11206ecbb08bff552c7d26b2ff74b7f8a6
ezequiel klug <ezeklug@gmail.com>
2020-08-29 12:26:03 -0300

    comenta2

--- TrabajoHP/InventarioBoutiqueDeFiestas/Controladores/ControladorCliente.cs
+++ TrabajoHP/InventarioBoutiqueDeFiestas/Controladores/ControladorCliente.cs
@@ -3,11 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using InventarioBoutiqueDeFiestas.Dominio;
+using InventarioBoutiqueDeFiestas.DTO;
 
 namespace InventarioBoutiqueDeFiestas.Controladores
 { 
     class ControladorCliente
     {
-    
+        /// <summary>
+        /// Este método permite agregar un cliente en la BD
+        /// </summary>
+        /// <param name="pClienteDTO"></param>
+        public void AgregarCliente(ClienteDTO pClienteDTO)
+        {
+
+        }
+        /// <summary>
+        /// Este método permite modificar un cliente ya agregado a BD
+        /// </summary>
+        /// <param name="pClienteDTO"></param>
+        public void ModificaCliente(ClienteDTO pClienteDTO)
+        {
+
+        }
+        /// <summary>
+        /// Este método permite realizar la Baja lógica de un cliente poniendo una propiedad "Activo" en Falso
+        /// </summary>
+        public void BajaCliente()
+        {
+
+        }
+        /// <summary>
+        /// Este método permite listar todos los clientes cargados en BD.
+        /// </summary>
+        /// <returns></returns>
+        public List<Cliente> ListarClientes()
+        {
+            return 
+        }
     }
 }
--- TrabajoHP/InventarioBoutiqueDeFiestas/Controladores/ControladorPresupuesto.cs
+++ TrabajoHP/InventarioBoutiqueDeFiestas/Controladores/ControladorPresupuesto.cs
@@ -17,47 +17,84 @@ namespace InventarioBoutiqueDeFiestas.Controladores
            
 
         }
+        /// <summary>
+        /// Método que permite agregar un presupuesto pasando sus parámetros para crearlo y guardarlo en db.
+        /// </summary>
+        /// <param name="presupuesto"></param>
         public void AgregarPresupuesto(PresupuestoDTO pPresupuesotDTO)
         {
-            
+
         }
+
+        /// <summary>
+        /// Método que permite agregar una línea correspondiente a un presupuesto y un producto, pasando los siguientes parámetros.
+        /// </summary>
+        /// <param name="pPrecioVenta"></param>
+        /// <param name="pCantidad"></param>
+        /// <param name="pDescripcion"></param>
+        /// <param name="pIdPresupuesto"></param>
+        /// <param name="pIdProducto"></param>
         public void AgregarLinea(LineaPresupuestoDTO pPresupuestoDTO)
         {
-           
+
         }
 
+        /// <summary>
+        /// Este metodo permite señar un presupuesto pasando como parámetro el id del presupuesto y el monto de la seña.
+        /// </summary>
+        /// <param name="pIdPresupuesto"></param>
+        /// <param name="pMontoSenia"></param>
         public void SeniarPresupuesto(int pIdPresupuesto, double pMontoSenia)
         {
-           
+
         }
 
+        /// <summary>
+        /// Este método permite vender un presupuesto, pasando como parámetro el id del presupuesto
+        /// </summary>
+        /// <param name="pIdPresupuesto"></param>
         public void Vender(int pIdPresupuesto)
         {
-            
+
         }
 
+        /// <summary>
+        /// Este metodo permite asociar un cliente a un presupuesto, pasando como parámetro el id del presupuesto y el id del cliente.
+        /// </summary>
+        /// <param name="pIdCliente"></param>
+        /// <param name="pIdPresupuesto"></param>
         public void AsociarCliente(int pIdCliente, int pIdPresupuesto)
         {
-            
+
         }
 
+        /// <summary>
+        /// Este método permite aplicar un descuento a todo un presupuesto, pasando como parámetro el id del presupuesto y el monto de descuento
+        /// </summary>
+        /// <param name="pIdPresupuesto"></param>
+        /// <param name="pDescuento"></param>
         public void AplicarDescuentoTotal(int pIdPresupuesto, float pDescuento)
         {
-            
+
         }
 
+        /// <summary>
+        /// Este método permite listar todos los presupuestos guardados en BD.
+        /// </summary>
+        /// <returns></returns>
         public List<Presupuesto> ListarPresupuesto()
         {
-            
+            return 
         }
-
+        /// <summary>
+        /// Este método permite aplicar un descuento a una linea de presupuesto, pasando como parámetro el id de la linea y el porcentaje de descuento.
+        /// </summary>
+        /// <param name="pIdPresupuesto"></param>
+        /// <param name="idProducto"></param>
+        /// <param name="pDescuento"></param>
         public void AplicarDescuentoLinea(int pIdLinea, float pDescuento)
         {
-            
+
         }
-
-
-
-
     }
 }
