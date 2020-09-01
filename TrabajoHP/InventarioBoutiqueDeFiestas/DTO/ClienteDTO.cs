67b32968b8c460da400ee95d6156689f45589f4e
ezequiel klug <ezeklug@gmail.com>
2020-09-01 16:32:02 -0300

    public dtos

--- TrabajoHP/InventarioBoutiqueDeFiestas/DTO/ClienteDTO.cs
+++ TrabajoHP/InventarioBoutiqueDeFiestas/DTO/ClienteDTO.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace InventarioBoutiqueDeFiestas.DTO
 {
-    class ClienteDTO
+    public class ClienteDTO
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
--- TrabajoHP/InventarioBoutiqueDeFiestas/DTO/LoteDTO.cs
+++ TrabajoHP/InventarioBoutiqueDeFiestas/DTO/LoteDTO.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace InventarioBoutiqueDeFiestas.DTO
 {
-    class LoteDTO
+    public class LoteDTO
     {
         public int Id { get; set; }
         public int CantidadProductos { get; set; }
