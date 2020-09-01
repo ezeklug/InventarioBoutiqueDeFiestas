18211137f3057c1b7e2b5ca736fe47311e32d678
ezequiel klug <ezeklug@gmail.com>
2020-09-01 18:05:33 -0300

    bajaProducto

--- TrabajoHP/InventarioBoutiqueDeFiestas/Controladores/ControladorProducto.cs
+++ TrabajoHP/InventarioBoutiqueDeFiestas/Controladores/ControladorProducto.cs
@@ -65,7 +65,11 @@ namespace InventarioBoutiqueDeFiestas.Controladores
         /// <param name="pIdProducto"></param>
         public void BajaProducto(int pIdProducto)
         {
-            throw new NotImplementedException();
+            using (Repositorio repo=new Repositorio())
+            {
+                Producto prod = repo.Productos.Find(pIdProducto);
+                prod.Activo = false;
+            }
         }
 
         /// <summary>
@@ -132,6 +136,5 @@ namespace InventarioBoutiqueDeFiestas.Controladores
                 }
             }
         }
-
     }
 }
