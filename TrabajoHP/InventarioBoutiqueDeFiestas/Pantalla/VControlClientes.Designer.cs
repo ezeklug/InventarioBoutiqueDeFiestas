684d72475c857601764985842774673d518c0107
ezequiel klug <ezeklug@gmail.com>
2020-09-04 16:06:00 -0300

    Pantalla

---
+++ TrabajoHP/InventarioBoutiqueDeFiestas/Pantalla/VControlClientes.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace InventarioBoutiqueDeFiestas
+{
+    partial class VControlClientes
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(229, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(317, 57);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Control Clientes";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(55, 81);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(678, 289);
+            this.checkedListBox1.TabIndex = 2;
+            // 
+            // VControlClientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "VControlClientes";
+            this.Text = "Control Clientes";
+            this.Load += new System.EventHandler(this.VControlClientes_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
+
---
+++ TrabajoHP/InventarioBoutiqueDeFiestas/Pantalla/VControlClientes.cs
@@ -0,0 +1,40 @@
+﻿using InventarioBoutiqueDeFiestas.Dominio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using InventarioBoutiqueDeFiestas.DTO;
+using InventarioBoutiqueDeFiestas.Controladores;
+using InventarioBoutiqueDeFiestas.Dominio;
+
+namespace InventarioBoutiqueDeFiestas
+{
+    public partial class VControlClientes : Form
+    {
+        ControladorFachada fachada = new ControladorFachada();
+        public VControlClientes()
+        {
+            InitializeComponent();
+        }
+
+        private void VControlClientes_Load(object sender, EventArgs e)
+        {
+            List<Cliente> listaCliente = fachada.ListarClientes();
+            foreach(Cliente cli in listaCliente)
+            {
+                int index = this.checkedListBox1.Items.Add(cli.Id);
+                checkedListBox1.Items[index] = cli.Id;
+                this.checkedListBox1.Items[1] = cli.Nombre;
+                this.checkedListBox1.Items[2] = cli.Apellido;
+                this.checkedListBox1.Items[3] = cli.Telefono;
+                this.checkedListBox1.Items[4] = cli.Direccion;
+                this.checkedListBox1.Items[5] = cli.Email;
+            }
+        }
+    }
+}
