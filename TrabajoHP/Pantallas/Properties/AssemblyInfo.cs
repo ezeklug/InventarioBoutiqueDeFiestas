8e9321f21dd9d1c9b9f5c6a689572f2d41c5a4a7
ezequiel klug <ezeklug@gmail.com>
2020-09-04 16:18:11 -0300

    a

---
+++ TrabajoHP/Pantallas/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Pantallas")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pantallas")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eaed8d62-fd39-49d1-906d-7f08f164e42c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TrabajoHP/Pantallas/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InventarioBoutiqueDeFiestas.Dominio;
+using InventarioBoutiqueDeFiestas.Database;
+using InventarioBoutiqueDeFiestas.Controladores;
+using InventarioBoutiqueDeFiestas.DTO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Npgsql;
+using InventarioBoutiqueDeFiestas;
+using System.Collections.Generic;
+
+namespace Pantallas
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestVControlClientes()
+        {
+            
+        }
+    }
+}
