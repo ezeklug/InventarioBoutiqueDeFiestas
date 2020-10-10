fcd336a67e1c223b0a54e1bb66d3fd5299d866e8
ezequiel klug <ezeklug@gmail.com>
2020-10-10 18:05:21 -0300

    j

--- TrabajoHP/InventarioBoutiqueDeFiestas/Ventanas/VSeniarPresupuesto.Designer.cs
+++ TrabajoHP/InventarioBoutiqueDeFiestas/Ventanas/VSeniarPresupuesto.Designer.cs
@@ -52,10 +52,10 @@
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold);
             this.label1.Location = new System.Drawing.Point(199, 23);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(356, 39);
+            this.label1.Size = new System.Drawing.Size(376, 39);
             this.label1.TabIndex = 0;
             this.label1.Text = "Señar un presupuesto";
             this.label1.Click += new System.EventHandler(this.label1_Click);
