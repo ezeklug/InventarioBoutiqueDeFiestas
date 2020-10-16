30e4bf866b61f4ba9192b36fea019ba5c94bdae6
Victoria <victoriaponsiano@gmail.com>
2020-10-16 17:22:20 -0300

    falta ExportarPresup

--- TrabajoHP/InventarioBoutiqueDeFiestas/Ventanas/VAdministrarPresupuesto.Designer.cs
+++ TrabajoHP/InventarioBoutiqueDeFiestas/Ventanas/VAdministrarPresupuesto.Designer.cs
@@ -49,6 +49,7 @@ namespace InventarioBoutiqueDeFiestas.Ventanas
             this.label5 = new System.Windows.Forms.Label();
             this.Principal = new System.Windows.Forms.Button();
             this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.botonExportar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
@@ -276,11 +277,27 @@ namespace InventarioBoutiqueDeFiestas.Ventanas
             // 
             this.errorProvider1.ContainerControl = this;
             // 
+            // botonExportar
+            // 
+            this.botonExportar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.botonExportar.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.botonExportar.FlatAppearance.BorderSize = 0;
+            this.botonExportar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botonExportar.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonExportar.Location = new System.Drawing.Point(374, 611);
+            this.botonExportar.Name = "botonExportar";
+            this.botonExportar.Size = new System.Drawing.Size(98, 23);
+            this.botonExportar.TabIndex = 37;
+            this.botonExportar.Text = "Exportar a pdf";
+            this.botonExportar.UseVisualStyleBackColor = false;
+            this.botonExportar.Click += new System.EventHandler(this.botonExportar_Click);
+            // 
             // VAdministrarPresupuesto
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(949, 671);
+            this.Controls.Add(this.botonExportar);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.Cancelar);
@@ -349,5 +366,6 @@ namespace InventarioBoutiqueDeFiestas.Ventanas
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Button botonExportar;
     }
 }
