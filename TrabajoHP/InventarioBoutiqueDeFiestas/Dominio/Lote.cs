6420b1d959b50f86b1153df1ed1fcd9570b0530f
leohoet98@gmail.com <leohoet98@gmail.com>
2020-10-30 16:39:13 -0300

    asd

--- TrabajoHP/InventarioBoutiqueDeFiestas/Dominio/Lote.cs
+++ TrabajoHP/InventarioBoutiqueDeFiestas/Dominio/Lote.cs
@@ -22,7 +22,9 @@ namespace InventarioBoutiqueDeFiestas.Dominio
             FechaCompra = DateTime.Now;
             FechaVencimiento = pFechaVencimiento;
             Producto = pProducto;
+            Vencido = false;
         }
+        
         public Lote() { }
     }
 }
