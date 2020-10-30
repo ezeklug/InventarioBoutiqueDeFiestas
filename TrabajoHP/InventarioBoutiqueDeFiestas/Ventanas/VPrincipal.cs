7d5f746bda9db4ecd9ae84c8a3a082de7aa80441
ezequiel klug <ezeklug@gmail.com>
2020-10-30 15:48:35 -0300

    notis

--- TrabajoHP/InventarioBoutiqueDeFiestas/Ventanas/VPrincipal.cs
+++ TrabajoHP/InventarioBoutiqueDeFiestas/Ventanas/VPrincipal.cs
@@ -31,6 +31,20 @@ namespace InventarioBoutiqueDeFiestas
         {
             //var cont = new ControladorFachada();
             //var not = cont.Notificaciones();
+            dataGridView1.Columns.Add("Fecha", "Fecha");
+            dataGridView1.Columns.Add("Descripcion", "Descripcion");
+            dataGridView1.Columns.Add("PresupuestoId", "PresupuestoId");
+            dataGridView1.Columns.Add("LoteId", "LoteId");
+            dataGridView1.Columns[2].Visible = false;
+            dataGridView1.Columns[3].Visible = false;
+            dataGridView1.Columns[0].ReadOnly = true;
+            dataGridView1.Columns[1].ReadOnly = true;
+            dataGridView1.Columns[2].ReadOnly = true;
+            dataGridView1.Columns[3].ReadOnly = true;
+            string[] r = new string[] { "25/12/2020", "Hola","5", "0"};
+            dataGridView1.Rows.Add(r);
+            string[] r2 = new string[] { "25/11/2015", "Hola2", "0", "7" };
+            dataGridView1.Rows.Add(r2);
         }
 
         private void ControlPresupuesto_Click(object sender, EventArgs e)
@@ -49,14 +63,9 @@ namespace InventarioBoutiqueDeFiestas
             this.Close();
         }
 
-        private void panel1_Paint(object sender, PaintEventArgs e)
+        private void CellDoubleClick(object sender, DataGridViewCellEventArgs e)
         {
-
-        }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
+            MessageBox.Show(dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString());
         }
     }
 }
